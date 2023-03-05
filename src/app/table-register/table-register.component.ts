a50165046c0bb23765a850e31d77496a962f0a9e
Jose305 <jaruiz17@espe.edu.ec>
2023-03-05 17:04:29 -0500

    Configuracion de la tabla registro

--- src/app/table-register/table-register.component.ts
+++ src/app/table-register/table-register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { EstudianteService } from '../estudiante.service';
-
+import { Estudiante } from '../model/Estudiante';
 @Component({
   selector: 'app-table-register',
   templateUrl: './table-register.component.html',
@@ -11,8 +11,9 @@ import { EstudianteService } from '../estudiante.service';
 export class TableRegisterComponent implements OnInit {
 
   datos: any[] = [];
+  estudiante:  any[] = [];
   constructor(private http: HttpClient, private estudianteService: EstudianteService, private router: Router) {
-    this.http.get<any[]>('http://44.202.153.93:8080/api/estudiante/').subscribe(data => {
+    this.http.get<any[]>('http://52.91.172.28:8080/api/estudiante/').subscribe(data => {
       this.datos = data;
     });
     console.log(this.datos);
@@ -28,9 +29,32 @@ export class TableRegisterComponent implements OnInit {
     this.router.navigate(['/inscripciones-aceptadas']);
   }
 
-  enviar(_id: string, estado_comprobante: string){
+  async enviar(_id: string, estado_comprobante: string, correo:string){
     this.estudianteService.actualizarAtributosEstudiante(_id, estado_comprobante).subscribe(res=>{
-      location.reload();
+      if(estado_comprobante == 'APROBADO'){
+          const to = ""+correo+"";
+          const subject = "Aprobación de registro";
+          const message = "Su solicitud a sido aprobada, continue con el proceso enviando el comprobante de pago accediendo al siguiente enlace: http://localhost:4200/registro-comprobante";
+
+          this.estudianteService.enviarcorreo(to, subject, message).subscribe(
+            res=>{
+              location.reload();
+            },(err) => console.log(err)
+          );
+      }else{
+        const to = ""+correo+"";
+        const subject = "Rechazo de registro";
+        const message = "Su registro a sido rechazado";
+
+        this.estudianteService.enviarcorreo(to, subject, message).subscribe(
+          res=>{
+            location.reload();
+          },(err) => console.log(err)
+        );
+      }
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
     },
     (err) => console.log(err)
     );
